c42d4103a829f9945dd3b3305e8245257f8751e0
sbanasiak <92038757+sbanasiak@users.noreply.github.com>
2023-04-19 15:43:53 +0200

    SvelteKit setup (#2)
    * Docker setup
    
    * Initialize SvelteKit project with Skeleton UI
    
    * Readme update

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
 # website
+
+This project uses [Docker](https://www.docker.com/) for development.
+
+To create a container for development, run in the root folder:
+```shell
+docker-compose up -d
+```
+
+You can now install the required packages and start the dev server:
+```shell
+docker-compose exec node npm install
+docker-compose exec node npm run dev
+```
---
+++ vite.config.ts
@@ -0,0 +1,19 @@
+import { sveltekit } from '@sveltejs/kit/vite';
+import { defineConfig } from 'vitest/config';
+
+export default defineConfig({
+	plugins: [sveltekit()],
+	server: {
+		host: "0.0.0.0",
+		port: 5173,
+		hmr: {
+			host: "localhost"
+		},
+		watch: {
+			usePolling: true,
+		},
+	},
+	test: {
+		include: ['src/**/*.{test,spec}.{js,ts}']
+	}
+});
