b47c48405ecd6c8d6c4f8214ce61c5de88b083ff
sbanasiak <seweryn.banasiak@studenci.collegiumwitelona.pl>
2023-04-29 23:26:42 +0200

    Add Firebase wrapper

---
+++ src/lib/firebase/User.svelte
@@ -0,0 +1,22 @@
+<script lang="ts">
+	import type { Auth, User } from 'firebase/auth';
+	import { sdk, userStore } from './stores';
+
+	interface $$Slots {
+		loading: object;
+		default: { user: User };
+		signedOut: object;
+	}
+
+	export let auth: Auth = $sdk?.auth;
+
+	const user = userStore(auth);
+</script>
+
+{#if $user}
+	<slot user={$user} />
+{:else if $user === undefined}
+	<slot name="loading" />
+{:else}
+	<slot name="signedOut" />
+{/if}
